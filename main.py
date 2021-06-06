d98a8ca6d22109aeba54ab79d4bc365b31a30e4a
danielcarvalho99 <danielcd3099@gmail.com>
2021-06-06 20:06:47 -0300

    adicionado constraste

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps,ImageEnhance
 from pymouse import PyMouse
 import time
 
 UPPER_LEFT = (457, 226)
 LOWER_BOTTOM = (731, 441)
-VALOR_MINIMO = 250
+VALOR_MINIMO = 73
 
 m = PyMouse()
 print(m.position())
 
-imagem = Image.open('mike.jpg')
+nome_da_imagem = input("Qual o arquivo da imagem: ")
+
+imagem = Image.open(nome_da_imagem)
 resize_image = imagem.resize((LOWER_BOTTOM[0] - UPPER_LEFT[0], LOWER_BOTTOM[1] - UPPER_LEFT[1]))
 
-imagem_gray = ImageOps.grayscale(resize_image)
+# Põe o contraste
+enhancer = ImageEnhance.Contrast(resize_image)
+imagem_contraste = enhancer.enhance(1.3)
+
+imagem_gray = ImageOps.grayscale(imagem_contraste)
 
 pixels = imagem_gray.load()
 
@@ -27,5 +33,5 @@ for x in range(width):
         
         if(imagem_gray.getpixel((x, y)) < VALOR_MINIMO):
             m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
-            time.sleep(0.0083)
+            time.sleep(0.0075)
         
