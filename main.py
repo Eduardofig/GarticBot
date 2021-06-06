7209055cdfe4a37d4f2dee72ccc0e598045b361e
danielcarvalho99 <danielcd3099@gmail.com>
2021-06-06 18:20:26 -0300

    tempo arrumado

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import time
 
 UPPER_LEFT = (457, 226)
 LOWER_BOTTOM = (731, 441)
-VALOR_MINIMO = 220
+VALOR_MINIMO = 250
 
 m = PyMouse()
 print(m.position())
 
-imagem = Image.open('pikachu.png')
+imagem = Image.open('mike.jpg')
 resize_image = imagem.resize((LOWER_BOTTOM[0] - UPPER_LEFT[0], LOWER_BOTTOM[1] - UPPER_LEFT[1]))
 
 imagem_gray = ImageOps.grayscale(resize_image)
@@ -19,11 +19,13 @@ pixels = imagem_gray.load()
 width, height = imagem_gray.size
 
 m = PyMouse()
+imagem_gray.show()
+
 
 for x in range(width):
     for y in range(height):
+        
         if(imagem_gray.getpixel((x, y)) < VALOR_MINIMO):
             m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
-            time.sleep(0.02)
-    time.sleep(0.4)
+            time.sleep(0.0083)
         
