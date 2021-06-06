0f5e8b77c17886360f348aca846b5b2954c5c49c
danielcarvalho99 <danielcd3099@gmail.com>
2021-06-06 17:45:23 -0300

    adicionado o sleep para nao travar o gartic

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from PIL import Image, ImageOps
 from pymouse import PyMouse
+import time
 
-UPPER_LEFT = (359, 488)
-LOWER_BOTTOM = (452, 576)
+UPPER_LEFT = (457, 226)
+LOWER_BOTTOM = (731, 441)
 VALOR_MINIMO = 220
 
-imagem = Image.open('anime.jpeg')
+m = PyMouse()
+print(m.position())
+
+imagem = Image.open('pikachu.png')
 resize_image = imagem.resize((LOWER_BOTTOM[0] - UPPER_LEFT[0], LOWER_BOTTOM[1] - UPPER_LEFT[1]))
 
 imagem_gray = ImageOps.grayscale(resize_image)
@@ -20,4 +24,6 @@ for x in range(width):
     for y in range(height):
         if(imagem_gray.getpixel((x, y)) < VALOR_MINIMO):
             m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+            time.sleep(0.02)
+    time.sleep(0.4)
         
