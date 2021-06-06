807f0964c9543038aec2f56fcf2b0681648d47a1
danielcarvalho99 <danielcd3099@gmail.com>
2021-06-06 20:19:09 -0300

    constrate posto como input do usuario

--- main.py
+++ main.py
@@ -14,24 +14,22 @@ nome_da_imagem = input("Qual o arquivo da imagem: ")
 imagem = Image.open(nome_da_imagem)
 resize_image = imagem.resize((LOWER_BOTTOM[0] - UPPER_LEFT[0], LOWER_BOTTOM[1] - UPPER_LEFT[1]))
 
+contraste = (float)(input("Quanto você quer de constraste: "))
 # Põe o contraste
 enhancer = ImageEnhance.Contrast(resize_image)
-imagem_contraste = enhancer.enhance(1.3)
+imagem_contraste = enhancer.enhance(contraste)
 
 imagem_gray = ImageOps.grayscale(imagem_contraste)
 
 pixels = imagem_gray.load()
 
 width, height = imagem_gray.size
-
 m = PyMouse()
-imagem_gray.show()
-
 
 for x in range(width):
     for y in range(height):
         
         if(imagem_gray.getpixel((x, y)) < VALOR_MINIMO):
             m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
-            time.sleep(0.0075)
-        
+            time.sleep(0.0083)
+
