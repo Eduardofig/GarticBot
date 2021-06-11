7f7aa63aecb0e72f76165b4da40e63b2490d14fb
Eduardofig <duzinho039@gmail.com>
2021-06-11 12:57:28 +0000

    draw dfs

--- main.py
+++ main.py
@@ -26,10 +26,36 @@ pixels = imagem_gray.load()
 width, height = imagem_gray.size
 m = PyMouse()
 
+row_vec = [1, 0, -1, 0]
+col_vec = [0, -1, 0, 1]
+visited = []
+for i in range(width):
+    col = []
+    for j in range(height):
+        col.append(False)
+    visited.append(col)
+
+def is_valid(x, y):
+    return imagem_gray.getpixel((x, y)) < VALOR_MINIMO and not visited[x][y] and x < width and y < height and x >= 0 and y >= 0
+
+def draw_dfs(x, y, pressed = False):
+    visited[x][y] = True
+    m.move(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+    if(not pressed): 
+        m.press(x, y)
+        pressed = True
+    backtrack = False
+    for i in range(4):
+        if(backtrack):
+            m.release(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+            pressed = False
+        if(is_valid(x + row_vec[i], y + col_vec[i])): 
+            draw_dfs(x + row_vec[i], y + col_vec[i], pressed)
+            backtrack = True
+
 for x in range(width):
     for y in range(height):
-        
-        if(imagem_gray.getpixel((x, y)) < VALOR_MINIMO):
-            m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+        if(is_valid(x, y)):
+            draw_dfs(x, y)
             time.sleep(0.0083)
 
