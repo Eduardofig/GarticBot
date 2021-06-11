77a71b56df7997f172b3290586b2e8158dee11b2
Eduardofig <duzinho039@gmail.com>
2021-06-11 19:26:39 +0000

    otimizado para um carai.2

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def draw_dfs(x, y):
             m.release(pos[0], pos[1])
             pressed = False
         if(is_valid(x + row_vec[i], y + col_vec[i])): 
-            time.sleep(0.0006)
+            time.sleep(0.0005)
             draw_dfs(x + row_vec[i], y + col_vec[i])
             backtrack = True
     pos = m.position()
