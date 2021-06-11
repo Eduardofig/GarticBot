7e18481538ab00930b20023d04d9af36b6184c9f
Eduardofig <duzinho039@gmail.com>
2021-06-11 13:51:01 +0000

    fixes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from PIL import Image, ImageOps,ImageEnhance
+from PIL import Image, ImageOps, ImageEnhance
 from pymouse import PyMouse
 import time
+import sys
+
+sys.setrecursionlimit(1050)
 
 UPPER_LEFT = (457, 226)
 LOWER_BOTTOM = (731, 441)
@@ -29,25 +32,28 @@ m = PyMouse()
 row_vec = [1, 0, -1, 0]
 col_vec = [0, -1, 0, 1]
 visited = []
-for i in range(width):
+for i in range(height):
     col = []
-    for j in range(height):
+    for j in range(width):
         col.append(False)
     visited.append(col)
 
 def is_valid(x, y):
-    return imagem_gray.getpixel((x, y)) < VALOR_MINIMO and not visited[x][y] and x < width and y < height and x >= 0 and y >= 0
+    if(x >= width or y >= height or x < 0 or y < 0): return False
+    return imagem_gray.getpixel((x, y)) < VALOR_MINIMO and not visited[y][x]
 
 def draw_dfs(x, y, pressed = False):
-    visited[x][y] = True
-    m.move(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+    time.sleep(0.01)
+    visited[y][x] = True
+    m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+    # m.move(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
     if(not pressed): 
-        m.press(x, y)
+        # m.press(x, y)
         pressed = True
     backtrack = False
     for i in range(4):
         if(backtrack):
-            m.release(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+            # m.release(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
             pressed = False
         if(is_valid(x + row_vec[i], y + col_vec[i])): 
             draw_dfs(x + row_vec[i], y + col_vec[i], pressed)
@@ -57,5 +63,5 @@ for x in range(width):
     for y in range(height):
         if(is_valid(x, y)):
             draw_dfs(x, y)
-            time.sleep(0.0083)
+            time.sleep(0.01)
 
