dda79bcb945edf3ca8e1479c30125a67e53c034e
Eduardofig <duzinho039@gmail.com>
2021-06-11 14:35:09 +0000

    dfs pronta

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from pymouse import PyMouse
 import time
 import sys
 
-sys.setrecursionlimit(1050)
+sys.setrecursionlimit(100000000)
 
-UPPER_LEFT = (457, 226)
-LOWER_BOTTOM = (731, 441)
+UPPER_LEFT = (114, 367)
+LOWER_BOTTOM = (443, 756)
 VALOR_MINIMO = 73
 
 m = PyMouse()
@@ -29,8 +29,8 @@ pixels = imagem_gray.load()
 width, height = imagem_gray.size
 m = PyMouse()
 
-row_vec = [1, 0, -1, 0]
-col_vec = [0, -1, 0, 1]
+row_vec = [1, 0, -1, 0, 1, 1, -1, -1]
+col_vec = [0, -1, 0, 1, 1, -1, -1, 1]
 visited = []
 for i in range(height):
     col = []
@@ -43,25 +43,26 @@ def is_valid(x, y):
     return imagem_gray.getpixel((x, y)) < VALOR_MINIMO and not visited[y][x]
 
 def draw_dfs(x, y, pressed = False):
-    time.sleep(0.01)
+    time.sleep(0.001)
     visited[y][x] = True
-    m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
-    # m.move(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+    # m.click(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+    m.move(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
     if(not pressed): 
-        # m.press(x, y)
+        m.press(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
         pressed = True
     backtrack = False
-    for i in range(4):
+    for i in range(6):
         if(backtrack):
-            # m.release(x + UPPER_LEFT[0], y + UPPER_LEFT[1])
+            m.release(m.position()[0], m.position()[1])
             pressed = False
         if(is_valid(x + row_vec[i], y + col_vec[i])): 
             draw_dfs(x + row_vec[i], y + col_vec[i], pressed)
             backtrack = True
+    m.release(m.position()[0], m.position()[1])
 
 for x in range(width):
     for y in range(height):
         if(is_valid(x, y)):
             draw_dfs(x, y)
-            time.sleep(0.01)
+            time.sleep(0.001)
 
